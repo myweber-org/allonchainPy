8425ee9144735812ba40cf06c2f9ea241fe6e035
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-20 23:08:32 +0000

    Add data cleaning utility for removing duplicate entries from a list

--- AocDesktop/data/screen/data_cleaner.py
+++ AocDesktop/data/screen/data_cleaner.py
@@ -1,9 +1,27 @@
-
-def remove_duplicates(seq):
+def remove_duplicates(input_list):
     seen = set()
     result = []
-    for item in seq:
+    for item in input_list:
         if item not in seen:
             seen.add(item)
             result.append(item)
     return result
+
+def clean_data_with_order(input_list, key=None):
+    if key is None:
+        key = lambda x: x
+    seen = set()
+    result = []
+    for item in input_list:
+        identifier = key(item)
+        if identifier not in seen:
+            seen.add(identifier)
+            result.append(item)
+    return result
+
+if __name__ == "__main__":
+    sample = [1, 2, 2, 3, 4, 4, 5]
+    print(remove_duplicates(sample))
+    
+    sample_complex = [{"id": 1}, {"id": 2}, {"id": 1}, {"id": 3}]
+    print(clean_data_with_order(sample_complex, key=lambda x: x["id"]))
