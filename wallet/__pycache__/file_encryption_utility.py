0dd6fdbf62153867a6c7e0114dd995cafdf027ea
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-24 00:06:47 +0000

    Add file encryption utility with AES-256-CBC mode

--- wallet/__pycache__/file_encryption_utility.py
+++ wallet/__pycache__/file_encryption_utility.py
@@ -1,85 +1,91 @@
 import os
 import base64
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
 
-class SecureFileEncryptor:
-    def __init__(self, salt_length=16, iterations=100000):
-        self.salt_length = salt_length
-        self.iterations = iterations
-        self.backend = default_backend()
+class FileEncryptor:
+    def __init__(self, password: str, salt: bytes = None):
+        self.password = password.encode()
+        self.salt = salt or os.urandom(16)
+        self.key = self._derive_key()
 
-    def derive_key(self, password, salt):
+    def _derive_key(self):
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
-            salt=salt,
-            iterations=self.iterations,
-            backend=self.backend
+            salt=self.salt,
+            iterations=100000,
+            backend=default_backend()
         )
-        return kdf.derive(password.encode())
+        return kdf.derive(self.password)
 
-    def encrypt_file(self, input_path, output_path, password):
-        salt = os.urandom(self.salt_length)
-        key = self.derive_key(password, salt)
-        
+    def encrypt_file(self, input_path: str, output_path: str):
         iv = os.urandom(16)
-        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=self.backend)
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=default_backend())
         encryptor = cipher.encryptor()
-        
-        with open(input_path, 'rb') as infile:
-            plaintext = infile.read()
-        
-        padding_length = 16 - (len(plaintext) % 16)
-        plaintext += bytes([padding_length]) * padding_length
-        
-        ciphertext = encryptor.update(plaintext) + encryptor.finalize()
-        
-        with open(output_path, 'wb') as outfile:
-            outfile.write(salt)
-            outfile.write(iv)
-            outfile.write(ciphertext)
+        padder = padding.PKCS7(128).padder()
 
-    def decrypt_file(self, input_path, output_path, password):
-        with open(input_path, 'rb') as infile:
-            salt = infile.read(self.salt_length)
-            iv = infile.read(16)
-            ciphertext = infile.read()
-        
-        key = self.derive_key(password, salt)
-        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=self.backend)
-        decryptor = cipher.decryptor()
-        
-        plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-        padding_length = plaintext[-1]
-        plaintext = plaintext[:-padding_length]
-        
-        with open(output_path, 'wb') as outfile:
-            outfile.write(plaintext)
+        with open(input_path, 'rb') as f_in, open(output_path, 'wb') as f_out:
+            f_out.write(self.salt)
+            f_out.write(iv)
+            
+            while True:
+                chunk = f_in.read(4096)
+                if not chunk:
+                    break
+                padded_data = padder.update(chunk)
+                encrypted_chunk = encryptor.update(padded_data)
+                f_out.write(encrypted_chunk)
+            
+            final_padded = padder.finalize()
+            final_encrypted = encryptor.update(final_padded) + encryptor.finalize()
+            f_out.write(final_encrypted)
 
-def generate_secure_password(length=32):
-    return base64.urlsafe_b64encode(os.urandom(length)).decode()[:length]
+    def decrypt_file(self, input_path: str, output_path: str):
+        with open(input_path, 'rb') as f_in:
+            salt = f_in.read(16)
+            iv = f_in.read(16)
+            
+            if salt != self.salt:
+                self.salt = salt
+                self.key = self._derive_key()
+            
+            cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=default_backend())
+            decryptor = cipher.decryptor()
+            unpadder = padding.PKCS7(128).unpadder()
+            
+            with open(output_path, 'wb') as f_out:
+                while True:
+                    chunk = f_in.read(4096)
+                    if not chunk:
+                        break
+                    decrypted_chunk = decryptor.update(chunk)
+                    unpadded_data = unpadder.update(decrypted_chunk)
+                    f_out.write(unpadded_data)
+                
+                final_decrypted = decryptor.finalize()
+                final_unpadded = unpadder.update(final_decrypted) + unpadder.finalize()
+                f_out.write(final_unpadded)
+
+def generate_random_key() -> str:
+    return base64.urlsafe_b64encode(os.urandom(32)).decode()
 
 if __name__ == "__main__":
-    encryptor = SecureFileEncryptor()
+    encryptor = FileEncryptor("secure_password_123")
     
     test_data = b"This is a secret message that needs encryption."
-    with open('test_plain.txt', 'wb') as f:
+    with open("test_original.txt", "wb") as f:
         f.write(test_data)
     
-    password = generate_secure_password()
-    print(f"Generated password: {password}")
+    encryptor.encrypt_file("test_original.txt", "test_encrypted.bin")
+    encryptor.decrypt_file("test_encrypted.bin", "test_decrypted.txt")
     
-    encryptor.encrypt_file('test_plain.txt', 'test_encrypted.bin', password)
-    encryptor.decrypt_file('test_encrypted.bin', 'test_decrypted.txt', password)
+    with open("test_decrypted.txt", "rb") as f:
+        print(f.read())
     
-    with open('test_decrypted.txt', 'rb') as f:
-        decrypted = f.read()
-    
-    print(f"Decryption successful: {decrypted == test_data}")
-    
-    for file in ['test_plain.txt', 'test_encrypted.bin', 'test_decrypted.txt']:
-        if os.path.exists(file):
-            os.remove(file)
+    os.remove("test_original.txt")
+    os.remove("test_encrypted.bin")
+    os.remove("test_decrypted.txt")
