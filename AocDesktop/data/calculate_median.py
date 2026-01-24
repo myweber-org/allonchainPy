c6327df27a48ceaf29a5e8cd7806e97af0287805
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-24 22:23:50 +0000

    Add function to calculate median of a list of numbers

---
+++ AocDesktop/data/calculate_median.py
@@ -0,0 +1,11 @@
+
+def calculate_median(numbers):
+    if not numbers:
+        return None
+    sorted_numbers = sorted(numbers)
+    n = len(sorted_numbers)
+    mid = n // 2
+    if n % 2 == 0:
+        return (sorted_numbers[mid - 1] + sorted_numbers[mid]) / 2
+    else:
+        return sorted_numbers[mid]
