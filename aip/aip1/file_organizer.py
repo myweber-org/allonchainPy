5732c321d23068363e04ad033ec75c78bfdd5736
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-28 23:17:58 +0000

    Add script to organize files by extension into categorized folders

---
+++ aip/aip1/file_organizer.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+import argparse
+
+def organize_files(directory):
+    if not os.path.isdir(directory):
+        print(f"Error: {directory} is not a valid directory.")
+        return
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+
+        if os.path.isfile(file_path):
+            _, extension = os.path.splitext(filename)
+            extension = extension.lower()
+
+            if extension:
+                folder_name = extension[1:] + "_files"
+            else:
+                folder_name = "no_extension_files"
+
+            target_folder = os.path.join(directory, folder_name)
+            os.makedirs(target_folder, exist_ok=True)
+
+            target_path = os.path.join(target_folder, filename)
+            shutil.move(file_path, target_path)
+            print(f"Moved: {filename} -> {folder_name}/")
+
+    print("File organization completed.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Organize files in a directory by their extensions.")
+    parser.add_argument("directory", help="Path to the directory to organize")
+    args = parser.parse_args()
+
+    organize_files(args.directory)
