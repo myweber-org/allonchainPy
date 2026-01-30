391a2cba62f4d333c98ced85af4fa0200ee2f202
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-30 22:30:36 +0000

    Add data cleaning utility to remove duplicate entries from a list

--- AocDesktop/data/icon/data_cleaner.py
+++ AocDesktop/data/icon/data_cleaner.py
@@ -40,4 +40,30 @@ def calculate_statistics(data, column):
         'max': data[column].max(),
         'count': data[column].count()
     }
-    return stats
+    return statsdef remove_duplicates(data_list):
+    seen = set()
+    result = []
+    for item in data_list:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
+
+def clean_data_with_order(data_list, key=None):
+    if key is None:
+        key = lambda x: x
+    seen = set()
+    result = []
+    for item in data_list:
+        identifier = key(item)
+        if identifier not in seen:
+            seen.add(identifier)
+            result.append(item)
+    return result
+
+if __name__ == "__main__":
+    sample = [1, 2, 2, 3, 4, 4, 5]
+    print(remove_duplicates(sample))
+    
+    sample_complex = [{"id": 1}, {"id": 2}, {"id": 1}, {"id": 3}]
+    print(clean_data_with_order(sample_complex, key=lambda x: x["id"]))
